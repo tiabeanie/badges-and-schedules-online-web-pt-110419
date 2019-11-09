4ea46e38fb596beda6e0eaaec700d808300c45b2
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-09 22:34:24 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker(name = Arel)
+  return "Hello, my name is #{name}."
+end
+
+# def batch_badge_creator(speakers)
+#  speaker_badges = []
+#  speakers.each do |name|
+#     speaker_badges.push (badge_maker(name))
+#  end
+#  return speaker_badges
+# end
+
+def batch_badge_creator(speakers)
+  speakers.map do |name|
+    badge_maker(name)
+  end
+end 
+
+def assign_rooms(speakers)
+  speakers.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers).map do |name|
+    puts name
+  end
+  assign_rooms(speakers).map do |name|
+    puts name
+  end
+end
